54cf2064d328ae1d9a8cac0c07ed54dd535639b9
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-05-08 12:58:48 -0400

    .gitignore and database connect mongo atlas

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_ATLAS_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
